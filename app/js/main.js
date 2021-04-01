2610af9d6083db077f987ed40a4aa923f2591dc3
mahmud-makhsudov <mahmudmakhsudov@gmail.com>
2021-04-01 19:57:59 +0500

    add pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,42 @@
-$(function(){
+const headerProfileWrapper = document.querySelectorAll('.header__profile-wrapper')
+
+function fnClick(item) {
+  item.addEventListener('click', function () {
+    item.classList.toggle('active')
+    item.parentElement.querySelector('.profile-box').classList.toggle('active')
+  })
+}
+
+headerProfileWrapper.forEach(fnClick)
 
 
 
+
+
+const tabsBtn = document.querySelectorAll('.profile-page__tab');
+const tabsItems = document.querySelectorAll('.profile-page__tab-content');
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+  item.addEventListener('click', function () {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+
+    if( ! currentBtn.classList.contains('active') ){
+
+      tabsBtn.forEach(function(item){
+        item.classList.remove('active');
+      });
   
-});
+      tabsItems.forEach(function(item){
+        item.classList.remove('active');
+      });
+  
+      currentBtn.classList.add('active');
+      currentTab.classList.add('active');
+    };
+  });
+}
+
